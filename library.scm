03435aff9f076aaeb54c17c2abf399249e54ff09
Xihua Yang <the19thhell@xihuas-mbp.home>
2014-10-18 18:34:16 -0400

    car cdr supported

--- library.scm
+++ library.scm
@@ -1,2 +1,23 @@
-(define (cadr l)
-  (car (cdr l)))
+(define (caar L)
+  (car (car L)))
+
+(define (cadr L)
+  (car (cdr L)))
+
+(define (cdar L)
+  (cdr (car L)))
+
+(define (cddr L)
+  (cdr (cdr L)))
+
+(define (caadr L)
+  (car (car (cdr L))))
+
+(define (caddr L)
+  (car (cdr (cdr L))))
+
+(define (cdadr L)
+  (cdr (car (cdr L))))
+
+(define (cadddr L)
+  (car (cdr (cdr (cdr L)))))
