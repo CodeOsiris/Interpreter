b11be17ea65aafdeb74aaadd078ed86bfcf7bf4e
Xihua Yang <the19thhell@xihuas-mbp.home>
2014-10-18 23:31:11 -0400

    newline, assoc, append supported.to-do: apply, map

--- library.scm
+++ library.scm
@@ -21,3 +21,19 @@
 
 (define (cadddr L)
   (car (cdr (cdr (cdr L)))))
+
+(define (newline)
+  (display #\newline))
+
+(define (assoc obj L)
+  (cond ((null? L) #f)
+		((pair? (car L))
+		 (cond ((eq? (caar L) obj) (car L))
+			   (else (assoc obj (cdr L)))
+			   ))
+		 ))
+
+(define (append L1 L2)
+  (cond ((null? L1) L2)
+		(else (cons (car L1) (append (cdr L1) L2)))
+		))
